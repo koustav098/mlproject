55bf397446c8d1159daaf8cf48bf4500462adc15
Koustav <koustavsinha405@gmail.com>
2026-06-07 00:22:20 +0530

    Model Trainer

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -9,7 +9,8 @@ from dataclasses import dataclass
 
 from src.component.data_transformation import DataTransformation
 from src.component.data_transformation import DataTransformationConfig
-print("FILE IS RUNNING")
+from src.component.model_trainer import ModelTrainerConfig
+from src.component.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -59,4 +60,8 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -3,20 +3,45 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
-
+from sklearn.metrics import r2_score
 from src.exception import CustomException
-
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-        
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
-    print("UTILS LOADED")
+
+# FIXED: Replaced 'model=model' with 'models' to accept your dictionary
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        # FIXED: Indented all processing lines so they stay INSIDE the for loop
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predict outcomes
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Calculate R2 scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # FIXED: Assigned score to the correct model name mapping
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
